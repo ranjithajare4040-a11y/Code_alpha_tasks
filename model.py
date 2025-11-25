bc382e8e05f4cdd91cc7985f2b2365a5aadd71ad
ranjithajare4040-a11y <ranjithajare4040@gmail.com>
2025-11-25 22:30:27 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(upload_to='profiles/', blank=True)
+    followers = models.ManyToManyField(User, related_name='following', blank=True)
+
+    def __str__(self):
+        return self.user.username
